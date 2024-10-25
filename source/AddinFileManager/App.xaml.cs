229cc93d5fc89d2b847c9fa4b28169047a2400b4
yanggenjie <yang_genjie@126.com>
2024-10-25 14:28:58 +0800

    reefactor: 降级到Net Framework 4.7.2

--- source/AddinFileManager/App.xaml.cs
+++ source/AddinFileManager/App.xaml.cs
@@ -1,5 +1,9 @@
-﻿using System.Configuration;
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
 using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
 using System.Windows;
 
 namespace AddinFileManager
@@ -10,5 +14,4 @@ namespace AddinFileManager
     public partial class App : Application
     {
     }
-
 }
